34feaf870991ede7120a22a8af3b89709f6d80ed
Josh Langner <josh.langner@gmail.com>
2022-07-12 20:17:51 -0500

    pass styling grom genre to app

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -7,39 +7,54 @@ const Genre = (props) => {
   let colorCoding 
 switch(props.name) {
   case 'Action':
+    colorCoding = '#0433ff'
   break
   case 'Adventure':
+    colorCoding = '#00fdff'
     break
   case 'Fantasy':
+    colorCoding = '#00f900'
     break
     case 'Crime':
+      colorCoding = '#ff40ff'
   break
   case 'Thriller':
+    colorCoding = '#ff9300'
     break
   case 'Science Fiction':
+    colorCoding = '#942192'
     break
     case 'Horror':
+      colorCoding = '#ff2600'
   break
   case 'Animation':
+    colorCoding = '#fffb00'
     break
   case 'Comedy':
+    colorCoding = '#73fcd6'
     break
     case 'Drama':
+      colorCoding = '#73fcd6'
   break
   case 'Family':
+    colorCoding = '#ff2f92'
     break
   case 'Music':
+    colorCoding = '#009193'
     break
   case 'Mystery':
+    colorCoding = '#a9a9a9'
     break
   case 'Horror':
+    colorCoding = '#d4fb79'
     break
   case 'Romance':
+    colorCoding = '#531b93'
     break
   }
   return (
     <div className='color-code'>
-      <p>{props.name}</p>
+      <p style={{backgroundColor: colorCoding, color: '#000000' }}>{props.name}</p>
     </div>
   )
 }
